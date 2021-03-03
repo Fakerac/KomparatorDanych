0e94c685e068e348faa344f5e00388a528719a69
Fakerac <micheal.labus@gmail.com>
2021-03-03 14:37:03 +0100

    Nowe dodatki.

---
+++ DataScience/Komparator.Designer.cs
@@ -0,0 +1,201 @@
+﻿
+namespace DataScience
+{
+    partial class Komparator
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Komparator));
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBoxFilepath1 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage4 = new System.Windows.Forms.TabPage();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            resources.ApplyResources(this.listBox1, "listBox1");
+            this.listBox1.Name = "listBox1";
+            // 
+            // listBoxFilepath1
+            // 
+            resources.ApplyResources(this.listBoxFilepath1, "listBoxFilepath1");
+            this.listBoxFilepath1.Name = "listBoxFilepath1";
+            // 
+            // button3
+            // 
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            resources.ApplyResources(this.button4, "button4");
+            this.button4.Name = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage4);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            resources.ApplyResources(this.tabControl1, "tabControl1");
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.label1);
+            this.tabPage1.Controls.Add(this.listBox1);
+            this.tabPage1.Controls.Add(this.button2);
+            this.tabPage1.Controls.Add(this.button4);
+            this.tabPage1.Controls.Add(this.button3);
+            this.tabPage1.Controls.Add(this.listBoxFilepath1);
+            resources.ApplyResources(this.tabPage1, "tabPage1");
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // tabPage2
+            // 
+            resources.ApplyResources(this.tabPage2, "tabPage2");
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage4
+            // 
+            this.tabPage4.Controls.Add(this.button1);
+            this.tabPage4.Controls.Add(this.label4);
+            this.tabPage4.Controls.Add(this.richTextBox3);
+            this.tabPage4.Controls.Add(this.label5);
+            this.tabPage4.Controls.Add(this.richTextBox4);
+            resources.ApplyResources(this.tabPage4, "tabPage4");
+            this.tabPage4.Name = "tabPage4";
+            this.tabPage4.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label4.Name = "label4";
+            // 
+            // richTextBox3
+            // 
+            resources.ApplyResources(this.richTextBox3, "richTextBox3");
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.ReadOnly = true;
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label5.Name = "label5";
+            // 
+            // richTextBox4
+            // 
+            resources.ApplyResources(this.richTextBox4, "richTextBox4");
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.ReadOnly = true;
+            // 
+            // tabPage3
+            // 
+            resources.ApplyResources(this.tabPage3, "tabPage3");
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // Komparator
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.tabControl1);
+            this.Name = "Komparator";
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage4.ResumeLayout(false);
+            this.tabPage4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.TextBox listBoxFilepath1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.TabPage tabPage4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ DataScience/Komparator.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.IO;
+using System.Media;
+
+
+namespace DataScience
+{
+
+    public partial class Komparator : Form
+    {
+        
+        //Tworzy nowy odtwarzacz dźwięku, z dźwiękiem powiadomienia Windowsa.
+        SoundPlayer errorSound = new SoundPlayer(@"C:\Windows\Media\Windows Notify System Generic.wav");
+
+        //To jest wybrana ścieżka plików JSON, z automatu jest uznawana za C:\Data. W przyszłości będzie to folder w którym znajduje się nasz komparator.
+        string enteredFilePath = @"C:\Data";
+
+        //To jest nasz wybrany plik JSON. Z automatu jego ścieżka to "null" (nie mylić z brakiem zawartości)
+        string chosenJson = "null";
+
+        //A oto zawartość naszego naszego wybranego pliku JSON, jeśli owy nie został załadowany, użytkownik zostanie o tym poinformowany
+        string chosenJsonContents = "No file selected.";
+        public Komparator()
+        {
+            InitializeComponent();
+
+        }
+
+        //Zbiera wszystkie pliki z końcówką .json w wybranej ścieżce, sortuje i pokazuje na liście.
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string[] filePaths = Directory.GetFiles(enteredFilePath, "*.json");
+            if (filePaths.Length > 0)
+            {
+                listBox1.Items.Clear();
+                listBox1.Items.AddRange(filePaths);
+            }
+            else
+            {
+                Console.WriteLine("No files found.");
+                listBox1.Items.Clear();
+            }
+        }
+
+        //Sprawdza czy została podana ścieżka, jak nie, to błąd. Jeśli tak, to zapisuje ją.
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (listBoxFilepath1.TextLength > 0)
+            {
+                enteredFilePath = listBoxFilepath1.Text;
+            }
+            else
+            {
+                Console.WriteLine("No path specified.");
+                errorSound.Play();
+                if (MessageBox.Show("Nie podano ścieżki plików.", "Błąd", MessageBoxButtons.OK) == DialogResult.OK)
+                {
+                }
+            }
+        }
+
+        //Wybrany plik JSON jest czytany, jego ścieżka oraz zawartość zapisywana.
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItems.Count > 0)
+            {
+                chosenJson = listBox1.SelectedItem.ToString();
+                chosenJsonContents = System.IO.File.ReadAllText(chosenJson);
+            }
+            else
+            {
+                Console.WriteLine("No JSON file chosen.");
+                errorSound.Play();
+                if (MessageBox.Show("Nie wybrano pliku JSON.", "Błąd", MessageBoxButtons.OK) == DialogResult.OK)
+                {
+                }
+            }
+        }
+
+        //Sprawdza czy został wybrany plik JSON w zakładce Wybierania, jeśli tak, wyczyszcza chwilową zawartość, a później pokazuje go w "Zaserializowany JSON".
+        //Jeśli nie, wyskakuje błąd
+        private void button1_Click(object sender, EventArgs e)
+        {
+                if (chosenJsonContents.Length > 0)
+                {
+                    richTextBox4.Clear();
+                    richTextBox4.AppendText(chosenJsonContents);
+                }
+                else
+                {
+                    Console.WriteLine("No JSON file chosen.");
+                    errorSound.Play();
+                    if (MessageBox.Show("Plik JSON nie ma zawartości", "Błąd", MessageBoxButtons.OK) == DialogResult.OK)
+                    {
+                    }
+                }
+            }
+
+    }
+}
