d26a1f9838f2dcdc4fe82882a906369411594e91
Fakerac <micheal.labus@gmail.com>
2021-03-03 19:04:37 +0100

    Nowe zmiany
    - Naprawiono crashowanie aplikacji w niektórych przypadkach
    
    - Dodano funkcje ErrorMessage();
    
    - Dodano boolean "bFileChosen"
    
    - Dodano Filestream "fs" przez który nie można otwierać załadowanego pliku.
    
    Usprawniono oraz uproszczono kod.

--- DataScience/Komparator.Designer.cs
+++ DataScience/Komparator.Designer.cs
@@ -37,8 +37,13 @@ namespace DataScience
             this.button4 = new System.Windows.Forms.Button();
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
             this.tabPage4 = new System.Windows.Forms.TabPage();
             this.button1 = new System.Windows.Forms.Button();
             this.label4 = new System.Windows.Forms.Label();
@@ -48,38 +53,52 @@ namespace DataScience
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
             this.tabPage4.SuspendLayout();
             this.SuspendLayout();
             // 
             // button2
             // 
-            resources.ApplyResources(this.button2, "button2");
+            this.button2.Location = new System.Drawing.Point(309, 121);
             this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(267, 38);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Załaduj";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
-            resources.ApplyResources(this.listBox1, "listBox1");
+            this.listBox1.Location = new System.Drawing.Point(6, 6);
             this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(245, 589);
+            this.listBox1.TabIndex = 3;
             // 
             // listBoxFilepath1
             // 
-            resources.ApplyResources(this.listBoxFilepath1, "listBoxFilepath1");
+            this.listBoxFilepath1.Location = new System.Drawing.Point(309, 59);
             this.listBoxFilepath1.Name = "listBoxFilepath1";
+            this.listBoxFilepath1.Size = new System.Drawing.Size(267, 20);
+            this.listBoxFilepath1.TabIndex = 4;
             // 
             // button3
             // 
-            resources.ApplyResources(this.button3, "button3");
+            this.button3.Location = new System.Drawing.Point(325, 85);
             this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(92, 30);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Zatwierdź";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button4
             // 
-            resources.ApplyResources(this.button4, "button4");
+            this.button4.Location = new System.Drawing.Point(81, 597);
             this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(88, 31);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Wybierz";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
@@ -89,32 +108,98 @@ namespace DataScience
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Controls.Add(this.tabPage4);
             this.tabControl1.Controls.Add(this.tabPage3);
-            resources.ApplyResources(this.tabControl1, "tabControl1");
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(1296, 660);
+            this.tabControl1.TabIndex = 7;
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.button8);
+            this.tabPage1.Controls.Add(this.button5);
             this.tabPage1.Controls.Add(this.label1);
             this.tabPage1.Controls.Add(this.listBox1);
             this.tabPage1.Controls.Add(this.button2);
             this.tabPage1.Controls.Add(this.button4);
             this.tabPage1.Controls.Add(this.button3);
             this.tabPage1.Controls.Add(this.listBoxFilepath1);
-            resources.ApplyResources(this.tabPage1, "tabPage1");
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
             this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(1288, 634);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Wybieranie JSON";
             this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(309, 165);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(267, 38);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "Odładuj";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(472, 85);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(85, 29);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Wyszukaj";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
             // label1
             // 
-            resources.ApplyResources(this.label1, "label1");
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(268, 31);
             this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(358, 25);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Wpisz lokalizację folderu z plikami JSON";
             // 
             // tabPage2
             // 
-            resources.ApplyResources(this.tabPage2, "tabPage2");
+            this.tabPage2.Controls.Add(this.button7);
+            this.tabPage2.Controls.Add(this.button6);
+            this.tabPage2.Controls.Add(this.richTextBox1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
             this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1288, 634);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Edytowanie JSON";
             this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(143, 6);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(99, 23);
+            this.button7.TabIndex = 2;
+            this.button7.Text = "Zapisz JSON";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(248, 6);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(99, 23);
+            this.button6.TabIndex = 1;
+            this.button6.Text = "Załaduj JSON";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(8, 32);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1262, 586);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
             // 
             // tabPage4
             // 
@@ -123,56 +208,87 @@ namespace DataScience
             this.tabPage4.Controls.Add(this.richTextBox3);
             this.tabPage4.Controls.Add(this.label5);
             this.tabPage4.Controls.Add(this.richTextBox4);
-            resources.ApplyResources(this.tabPage4, "tabPage4");
+            this.tabPage4.Location = new System.Drawing.Point(4, 22);
             this.tabPage4.Name = "tabPage4";
+            this.tabPage4.Size = new System.Drawing.Size(1288, 634);
+            this.tabPage4.TabIndex = 3;
+            this.tabPage4.Text = "Serializacja JSON";
             this.tabPage4.UseVisualStyleBackColor = true;
             // 
             // button1
             // 
-            resources.ApplyResources(this.button1, "button1");
+            this.button1.Location = new System.Drawing.Point(248, 6);
             this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Załaduj JSON";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label4
             // 
-            resources.ApplyResources(this.label4, "label4");
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
             this.label4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label4.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label4.Location = new System.Drawing.Point(8, 314);
             this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(169, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Odserializowany JSON";
             // 
             // richTextBox3
             // 
-            resources.ApplyResources(this.richTextBox3, "richTextBox3");
+            this.richTextBox3.Location = new System.Drawing.Point(6, 337);
             this.richTextBox3.Name = "richTextBox3";
             this.richTextBox3.ReadOnly = true;
+            this.richTextBox3.Size = new System.Drawing.Size(1262, 273);
+            this.richTextBox3.TabIndex = 6;
+            this.richTextBox3.Text = "";
             // 
             // label5
             // 
-            resources.ApplyResources(this.label5, "label5");
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
             this.label5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label5.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label5.Location = new System.Drawing.Point(8, 9);
             this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(203, 20);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Raw JSON - Zserializowany";
             // 
             // richTextBox4
             // 
-            resources.ApplyResources(this.richTextBox4, "richTextBox4");
+            this.richTextBox4.Location = new System.Drawing.Point(6, 32);
             this.richTextBox4.Name = "richTextBox4";
             this.richTextBox4.ReadOnly = true;
+            this.richTextBox4.Size = new System.Drawing.Size(1262, 273);
+            this.richTextBox4.TabIndex = 4;
+            this.richTextBox4.Text = "";
             // 
             // tabPage3
             // 
-            resources.ApplyResources(this.tabPage3, "tabPage3");
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
             this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Size = new System.Drawing.Size(1288, 634);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Porównywanie Danych";
             this.tabPage3.UseVisualStyleBackColor = true;
             // 
             // Komparator
             // 
-            resources.ApplyResources(this, "$this");
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1286, 652);
             this.Controls.Add(this.tabControl1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Komparator";
+            this.Text = "Komparator";
             this.tabControl1.ResumeLayout(false);
             this.tabPage1.ResumeLayout(false);
             this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
             this.tabPage4.ResumeLayout(false);
             this.tabPage4.PerformLayout();
             this.ResumeLayout(false);
@@ -196,6 +312,11 @@ namespace DataScience
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.RichTextBox richTextBox4;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
     }
 }
 
--- DataScience/Komparator.cs
+++ DataScience/Komparator.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using Newtonsoft.Json;
 using System.IO;
 using System.Media;
+using System.Text.RegularExpressions;
 
 
 namespace DataScience
@@ -17,38 +18,63 @@ namespace DataScience
 
     public partial class Komparator : Form
     {
-        
+
+        //Boolean który sprawdza czy jakikolwiek plik został wybrany.
+        bool bFileChosen = false;
+
+        //File Stream który powoduje że wybrany plik nie może być edytowany.
+        FileStream fs;
+
         //Tworzy nowy odtwarzacz dźwięku, z dźwiękiem powiadomienia Windowsa.
         SoundPlayer errorSound = new SoundPlayer(@"C:\Windows\Media\Windows Notify System Generic.wav");
 
         //To jest wybrana ścieżka plików JSON, z automatu jest uznawana za C:\Data. W przyszłości będzie to folder w którym znajduje się nasz komparator.
         string enteredFilePath = @"C:\Data";
+        
 
         //To jest nasz wybrany plik JSON. Z automatu jego ścieżka to "null" (nie mylić z brakiem zawartości)
         string chosenJson = "null";
 
         //A oto zawartość naszego naszego wybranego pliku JSON, jeśli owy nie został załadowany, użytkownik zostanie o tym poinformowany
         string chosenJsonContents = "No file selected.";
+
+        public void ErrorMessage(string consolemsg, string msgboxmsg, string msgboxtitle)
+        {
+            Console.WriteLine(consolemsg);
+            errorSound.Play();
+            if (MessageBox.Show(msgboxmsg, msgboxtitle, MessageBoxButtons.OK) == DialogResult.OK)
+            {
+            }
+        }
         public Komparator()
         {
             InitializeComponent();
 
+            listBoxFilepath1.Clear();
+            listBoxFilepath1.AppendText(enteredFilePath);
+        }
+
+        //Sprawdza czy ścieżka pliku jest właściwa
+        static public bool isValidPath (string path)
+        {
+            Regex r = new Regex(@"^(([a-zA-Z]:)|(\))(\{1}|((\{1})[^\]([^/:*?<>""|]*))+)$");
+            return r.IsMatch(path);
         }
 
         //Zbiera wszystkie pliki z końcówką .json w wybranej ścieżce, sortuje i pokazuje na liście.
         private void button2_Click(object sender, EventArgs e)
         {
             string[] filePaths = Directory.GetFiles(enteredFilePath, "*.json");
-            if (filePaths.Length > 0)
-            {
-                listBox1.Items.Clear();
-                listBox1.Items.AddRange(filePaths);
-            }
-            else
-            {
-                Console.WriteLine("No files found.");
-                listBox1.Items.Clear();
-            }
+                if (filePaths.Length > 0)
+                {
+                    listBox1.Items.Clear();
+                    listBox1.Items.AddRange(filePaths);
+                }
+                else
+                {
+                    Console.WriteLine("No files found.");
+                    listBox1.Items.Clear();
+                }
         }
 
         //Sprawdza czy została podana ścieżka, jak nie, to błąd. Jeśli tak, to zapisuje ją.
@@ -56,15 +82,14 @@ namespace DataScience
         {
             if (listBoxFilepath1.TextLength > 0)
             {
-                enteredFilePath = listBoxFilepath1.Text;
+                if (isValidPath(listBoxFilepath1.Text))
+                {
+                    enteredFilePath = listBoxFilepath1.Text;
+                }
             }
             else
             {
-                Console.WriteLine("No path specified.");
-                errorSound.Play();
-                if (MessageBox.Show("Nie podano ścieżki plików.", "Błąd", MessageBoxButtons.OK) == DialogResult.OK)
-                {
-                }
+                ErrorMessage("No path specified", "Nie podano ścieżki plików.", "Błąd");
             }
         }
 
@@ -73,16 +98,21 @@ namespace DataScience
         {
             if (listBox1.SelectedItems.Count > 0)
             {
-                chosenJson = listBox1.SelectedItem.ToString();
-                chosenJsonContents = System.IO.File.ReadAllText(chosenJson);
+                if (bFileChosen != true)
+                {
+                    chosenJson = listBox1.SelectedItem.ToString();
+                    chosenJsonContents = System.IO.File.ReadAllText(chosenJson);
+                    fs = new FileStream(chosenJson, FileMode.Open, FileAccess.Read, FileShare.None);
+                    bFileChosen = true;
+                }
+                else if (bFileChosen == true)
+                {
+                    ErrorMessage("JSON file already in use.", "Plik JSON został już wybrany.", "Błąd");
+                }
             }
             else
             {
-                Console.WriteLine("No JSON file chosen.");
-                errorSound.Play();
-                if (MessageBox.Show("Nie wybrano pliku JSON.", "Błąd", MessageBoxButtons.OK) == DialogResult.OK)
-                {
-                }
+                ErrorMessage("No JSON file chosen.", "Nie wybrano pliku JSON.", "Błąd");
             }
         }
 
@@ -94,16 +124,43 @@ namespace DataScience
                 {
                     richTextBox4.Clear();
                     richTextBox4.AppendText(chosenJsonContents);
-                }
+
+            }
                 else
                 {
-                    Console.WriteLine("No JSON file chosen.");
-                    errorSound.Play();
-                    if (MessageBox.Show("Plik JSON nie ma zawartości", "Błąd", MessageBoxButtons.OK) == DialogResult.OK)
-                    {
-                    }
+                ErrorMessage("No JSON file contents.", "Plik JSON nie ma zawartości", "Błąd");
                 }
             }
 
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            fbd.RootFolder = Environment.SpecialFolder.Desktop;
+            fbd.Description = "Wybierz Folder";
+            fbd.ShowNewFolderButton = false;
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                listBoxFilepath1.Clear();
+                listBoxFilepath1.AppendText(fbd.SelectedPath);
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (bFileChosen == true)
+            {
+                fs.Close();
+                chosenJson = "null";
+                chosenJsonContents = "No file selected.";
+                listBox1.Items.Clear();
+                bFileChosen = false;
+            } else
+            {
+                listBox1.Items.Clear();
+            }
+        }
     }
 }
